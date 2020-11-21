7bd2e9b53a6f6f05f94389e9aa72507fa845506e
Hà Ngọc Kiên <62798483+kienyataome99@users.noreply.github.com>
2020-11-21 11:10:24 +0700

    Updates

---
+++ precache-manifest.305bb4ab961d96d22a216e6ab885d241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6c229624a5f55459d9c7819c31f653",
+    "url": "/st32020group12.tk/index.html"
+  },
+  {
+    "revision": "127778912b4712759640",
+    "url": "/st32020group12.tk/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "6f0c2b7655e25974c4fa",
+    "url": "/st32020group12.tk/static/css/main.d035ca55.chunk.css"
+  },
+  {
+    "revision": "127778912b4712759640",
+    "url": "/st32020group12.tk/static/js/2.10b3f2e5.chunk.js"
+  },
+  {
+    "revision": "18f8b9d96eb235c330898f10252c7e2e",
+    "url": "/st32020group12.tk/static/js/2.10b3f2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0c2b7655e25974c4fa",
+    "url": "/st32020group12.tk/static/js/main.2759f0d1.chunk.js"
+  },
+  {
+    "revision": "9ac813436e2665391c55",
+    "url": "/st32020group12.tk/static/js/runtime-main.b2c2fea7.js"
+  }
+]);
